c6b07e909628f704b8c8416f0cb769f19015b4eb
Callum Houghton <callum.houghton13@hotmail.co.uk>
2019-01-25 17:26:45 +0000

    Further work on molecule selection list, generating buttons dynamically

--- Assets/ChemView Scripts/MolListViewGenerator.cs
+++ Assets/ChemView Scripts/MolListViewGenerator.cs
@@ -1,19 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MolListViewGenerator : MonoBehaviour
 {
 
     public GameObject MoleculeContainer;
+    public GameObject ChemViewARControllerOBJ;
+    List<Button> moleculeButtons = new List<Button>();
+    GameObject buttonPrefab;
+    List<GameObject> molsList = new List<GameObject>();
     // Use this for initialization
     void Start()
     {
-        List<GameObject> molsList = new List<GameObject>();
         foreach (Transform molecule in MoleculeContainer.transform)
         {
+            GameObject molButton = Instantiate(buttonPrefab);
+            var button = GetComponent<Button>();
+            button.GetComponent<Text>().text = molecule.name;
+            button.onClick.AddListener(() => MoleculeClick(molecule.name));
+
             molsList.Add(molecule.gameObject);
         }
+    }
+
+    public void MoleculeClick(string molClicked)
+    {
+        GameObject newSelectedMol = molsList.Where(mol => mol.name == molClicked).FirstOrDefault();
+        ChemViewARController ChemController = ChemViewARControllerOBJ.GetComponent<ChemViewARController>();
+        ChemController.loadedChemModel = newSelectedMol;
     }
 
     void GenerateListItems( List<GameObject>molsList)
