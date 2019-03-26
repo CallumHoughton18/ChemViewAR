067db4d59f4093f7807c7802a423b14f01973c6b
Callum Houghton <callum.houghton13@hotmail.co.uk>
2019-03-26 21:12:02 +0000

    Improved searching for planes UI pop up.

--- Assets/ChemView Scripts/SearchingForPlaneController.cs
+++ Assets/ChemView Scripts/SearchingForPlaneController.cs
@@ -8,18 +8,19 @@ public class SearchingForPlaneController : MonoBehaviour
 {
 
     public Text searchingText;
-    bool _showDialog = false;
+    bool _showDialog = true;
     private float _visibleYPos;
     private float _hiddenYPos;
     private float _movementIncrement;
     List<string> searchingMessages;
-    int i = 0;
+    int messageIndex = 0;
     void Start() //TOOD: add way to cancel couritine
     {
         searchingMessages = new List<string> { "Searching for Plane", "Searching for Plane.", "Searching for Plane..", "Searching for Plane...", "Point at a Flat Surface to Generate a Plane!" };
         StartMessageCoroutine();
-        _visibleYPos = transform.position.y;
         RectTransform rt = GetComponent<RectTransform>();
+        _visibleYPos = rt.transform.position.y + 5;
+        transform.position = new Vector3(transform.position.x, _visibleYPos, transform.position.z);
         _hiddenYPos = _visibleYPos - rt.rect.height / 2;
     }
 
@@ -35,9 +36,16 @@ public class SearchingForPlaneController : MonoBehaviour
                 gameObject.SetActive(false);
             }
         }
-        else if (_showDialog && gameObject.transform.position.y <= _visibleYPos)
+        else if (_showDialog && gameObject.transform.position.y < _visibleYPos)
         {
             gameObject.transform.position = new Vector3(transform.position.x, transform.position.y + _movementIncrement, transform.position.z);
+        }
+
+        else if (_showDialog)
+        {
+            {
+                gameObject.transform.position = new Vector3(transform.position.x, _visibleYPos, transform.position.z);
+            }
         }
     }
 
@@ -48,11 +56,14 @@ public class SearchingForPlaneController : MonoBehaviour
         {
             gameObject.SetActive(true);
             _movementIncrement = Mathf.Abs((_visibleYPos - transform.position.y) / 3); //not completely at position it was originally in still?
+            messageIndex = 0;
+            //StartMessageCoroutine();
         }
 
         else
         {
             _movementIncrement = Mathf.Abs((_hiddenYPos - transform.position.y) / 3);
+            //StopCoroutine("SetText");
         }
 
         _showDialog = showDialog;
@@ -62,24 +73,22 @@ public class SearchingForPlaneController : MonoBehaviour
     public void StartMessageCoroutine()
     {
         searchingText.text = searchingMessages[0];
-        StartCoroutine(SetText());
-
-
+        StartCoroutine("SetText");
 
     }
 
     IEnumerator SetText()
     {
-        while (gameObject.activeSelf)
+        while (_showDialog)
         {
-            string message = searchingMessages[i];
+            string message = searchingMessages[messageIndex];
 
             searchingText.text = message;
-            i++;
+            messageIndex++;
 
-            if (i > searchingMessages.Count - 1)
+            if (messageIndex > searchingMessages.Count - 1)
             {
-                i = 0;
+                messageIndex = 0;
                 searchingText.alignment = TextAnchor.MiddleCenter;
                 yield return new WaitForSeconds(3f);
             }
